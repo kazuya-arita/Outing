cacb97e3b417b50573bec0a028302f54f7943b3b
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-10-03 14:22:13 +0000

    ゲストログイン

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "c9c53a389f5541599cd82ae11023183b.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
+  
 end
