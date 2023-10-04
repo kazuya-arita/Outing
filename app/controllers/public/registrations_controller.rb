fe3c520365a3e96b0bdf75a1377a8dce263f85a4
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-10-04 07:00:13 +0000

    新規登録入力項目追加

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +62,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :user_name])
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create!(
-  email: "ebiebi@.com", ## 管理者のメールアドレス
+  email: "ebiebi@ebi.com", ## 管理者のメールアドレス
   password: "ebiebi" ##パスワード
 )
