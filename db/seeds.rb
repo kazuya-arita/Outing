58c396b75f94da371f7d346df157780a807d0227
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-10-04 05:18:55 +0000

    次回ログイン実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: "ebiebi@.com", ## 管理者のメールアドレス
+  password: "ebiebi" ##パスワード
+)
