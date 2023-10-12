25dbed2bf05e81a06e962b4921b23b9702af0e40
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-10-12 13:08:02 +0000

    次回管理者投稿詳細

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,24 @@
 class Admin::UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @post_items = @user.post_items
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_path
+  end  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:is_acti)
+  end
+  
 end
