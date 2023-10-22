95617af1ab787ffe5467d2731964992ef334c9aa
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-10-22 01:25:05 +0000

    通知機能実装中

--- app/models/post_item.rb
+++ app/models/post_item.rb
@@ -30,4 +30,26 @@ class PostItem < ApplicationRecord
     end
   end
   
+  def create_notification_favorite!(current_user)
+    #すでにいいねされているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_item_id = ? and action = ? ", current_user.id, user_id, id, 'favorite'])
+    
+    if temp.blank?
+      notification = current_user.active_notificatons.new(post_id: id, visited_id: user_id, action: favorite)
+      #自分が投稿したものに自分でいいねした場合は通知済みにする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end  
+  end
+  
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(post_item_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end  
+  
 end
